871807591538a19a2f1f072d29d8345b043a6377
Seray Fırıncıoğulları <158742686+serayfirinciogullari@users.noreply.github.com>
2024-05-18 15:37:47 +0300

    kodun okunurluğu artırıldı ufak hatalar düzeltildi

--- istatistik.py
+++ istatistik.py
@@ -1,30 +1,24 @@
 import string
 
 class Istatistik:
+
     def __init__(self, dosya_adi):
         self.dosya_adi = dosya_adi
 
     def noktalama_sil(self, metin):
         noktalama_isaretleri = string.punctuation
-        temiz_metin = ""
-        for karakter in metin:
-            if karakter not in noktalama_isaretleri:
-                temiz_metin += karakter
+        temiz_metin = "".join([karakter for karakter in metin if karakter not in noktalama_isaretleri])
         return temiz_metin
 
     def bosluk_sil(self, metin):
         return "".join(metin.split())
-    
-    def bosluklara_ayir(self, metin):
-        return metin.split()
-
 
     def harf_sayisi_bul(self):
         toplam = 0
         harf = []
         sayi = []
 
-        with open(self.dosya_adi, "r+") as dosya:
+        with open(self.dosya_adi, "r+", encoding="utf-8") as dosya:
             icerik = dosya.read()
             yeni_icerik = self.noktalama_sil(icerik)
             yeni_icerik2 = self.bosluk_sil(yeni_icerik)
@@ -35,26 +29,24 @@ class Istatistik:
                     sayi.append(1)
                 else:
                     sayi[harf.index(i)] += 1
-            
+
             for j in range(len(harf)):
                 toplam += sayi[j]
-                
+
         print("Dosyadaki toplam harf sayısı -->", toplam)
 
-
     def kelime_sayisi_bul(self):
-        with open(self.dosya_adi, "r+") as dosya:
+        with open(self.dosya_adi, "r+", encoding="utf-8") as dosya:
             icerik = dosya.read()
             kelimeler = self.bosluklara_ayir(icerik)
             toplam = len(kelimeler)
-            print("Girilen dosyadaki toplam kelime sayısı-->", toplam)
-
+            print("Girilen dosyadaki toplam kelime sayısı -->", toplam)
 
     def etkisiz_kelime_sayisi(self):
-        etkisiz_kelimeler = ["ve", "veya", "ama", "ancak", "şu", "bu", "bir", "şey", "olarak", "olduğunu", "gibi", "ise", "ile", "o", "ki", "ile", "ile", "değil", "ise", "hepsi", "şöyle", "böyle", "şimdilik", "şöylelikle", "böylelikle", "zira", "çünkü", "şayet", "eğer", "her", "hep", "sadece", "yalnızca", "sanki", "mış", "miş", "muş", "müş", "diye", "oldu", "olduğu", "de", "da", "ile", "üzerinde", "altında", "içinde", "ardından", "sonra", "şimdi"]
+        etkisiz_kelimeler = ["ve", "veya", "ama", "ancak", "şu", "bu", "bir", "şey", "olarak", "olduğunu", "gibi", "ise", "ile", "o", "ki", "değil", "hepsi", "şöyle", "böyle", "şimdilik", "şöylelikle", "böylelikle", "zira", "çünkü", "şayet", "eğer", "her", "hep", "sadece", "yalnızca", "sanki", "mış", "miş", "muş", "müş", "diye", "oldu", "olduğu", "de", "da", "üzerinde", "altında", "içinde", "ardından", "sonra", "şimdi"]
         toplam = 0
 
-        with open(self.dosya_adi, "r+") as dosya:
+        with open(self.dosya_adi, "r+", encoding="utf-8") as dosya:
             icerik = dosya.read()
             yeni_icerik = self.bosluklara_ayir(self.noktalama_sil(icerik))
 
@@ -62,29 +54,27 @@ class Istatistik:
                 if kelime in etkisiz_kelimeler:
                     toplam += 1
 
-            for j in etkisiz_kelimeler:
-                if yeni_icerik.count(j) > 1:
-                    toplam += 1
+        print("Girilen dosyadaki etkisiz kelime sayısı -->", toplam)
 
-        print("Girilen dosyadaki etkisiz kelime sayısı-->", toplam)
-
-   @staticmethod
+    @staticmethod
     def hizli_sirala(liste):
         if len(liste) <= 1:
             return liste
 
         pivot = liste[len(liste) // 2]
-        küçükler = [x for x in liste if x < pivot]
-        orta = [x for x in liste if x == pivot]
-        büyükler = [x for x in liste if x > pivot]
+        küçükler = [x for x in liste if x[1] < pivot[1]]
+        orta = [x for x in liste if x[1] == pivot[1]]
+        büyükler = [x for x in liste if x[1] > pivot[1]]
 
-        return Istatistik.hizli_sirala(küçükler) + orta + Istatistik.hizli_sirala(büyükler)
-    
+        return Istatistik.hizli_sirala(büyükler) + orta + Istatistik.hizli_sirala(küçükler)
+
+    def bosluklara_ayir(self, metin):
+        return metin.split()
 
     def kelime_kullanim_sayilari(self):
         kelime_sayilari = {}
 
-        with open(self.dosya_adi, "r+", encoding="utf-8") as dosya:
+        with open(self.dosya_adi, "r", encoding="utf-8") as dosya:
             icerik = dosya.read()
             kelimeler = self.bosluklara_ayir(icerik)
 
@@ -96,11 +86,17 @@ class Istatistik:
 
         siralanmis_kelimeler = self.hizli_sirala(list(kelime_sayilari.items()))
 
-        print("\nEn çok kullanılan ilk 5 kelime:")
+        print("\nEn az kullanılan ilk 5 kelime:")
         for kelime, sayi in siralanmis_kelimeler[-5:]:
             print(f"{kelime}: {sayi}")
 
-        print("\nEn az kullanılan ilk 5 kelime:")
+        print("\nEn çok kullanılan ilk 5 kelime:")
         for kelime, sayi in siralanmis_kelimeler[:5]:
             print(f"{kelime}: {sayi}")
 
+# Örnek kullanım
+istatistik = Istatistik("deneme.txt")
+istatistik.harf_sayisi_bul()
+istatistik.kelime_sayisi_bul()
+istatistik.etkisiz_kelime_sayisi()
+istatistik.kelime_kullanim_sayilari()
